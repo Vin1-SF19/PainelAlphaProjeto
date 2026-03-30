48239e99d995e35ee2797d6d18080363d38a74ca
Alpha <ti.alphacomex@gmail.com>
2026-03-30 15:48:08 -0300

    middleware corrigido?

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,35 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-const { auth } = NextAuth(authConfig);
-
-export default auth((req) => {
-  const session = req.auth;
+export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  const isLoggedIn = !!session;
 
-  if (pathname.startsWith("/PainelAlpha/cadastro") && session?.user?.role !== "Admin") {
+  const token = await getToken({ 
+    req, 
+    secret: process.env.AUTH_SECRET,
+    cookieName: process.env.NODE_ENV === "production" 
+      ? "__Secure-next-auth.session-token" 
+      : "next-auth.session-token"
+  });
+
+  const isLoggedIn = !!token;
+
+  if (pathname === "/") return NextResponse.next();
+
+  if (!isLoggedIn && pathname.startsWith("/PainelAlpha")) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  if (
+    pathname.startsWith("/PainelAlpha/cadastro") &&
+    token?.role !== "Admin"
+  ) {
     return NextResponse.redirect(new URL("/PainelAlpha", req.nextUrl));
   }
 
   return NextResponse.next();
-});
+}
 
 export const config = {
   matcher: ["/PainelAlpha/:path*"],
